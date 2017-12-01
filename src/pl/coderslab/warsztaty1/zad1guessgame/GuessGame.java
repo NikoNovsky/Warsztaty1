74ce5cd8040db89d6046ca78dedfeb23a39757e1
NikoNovsky <nikonovsky@gmail.com>
2017-12-01 12:57:53 +0100

    create GuessGame.java and zad2lotto.java

---
+++ src/pl/coderslab/warsztaty1/zad1guessgame/GuessGame.java
@@ -0,0 +1,8 @@
+package pl.coderslab.warsztaty1.zad1guessgame;
+
+public class GuessGame {
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ src/pl/coderslab/warsztaty1/zad2lotto/zad2lotto.java
@@ -0,0 +1,10 @@
+package pl.coderslab.warsztaty1.zad2lotto;
+
+public class zad2lotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
